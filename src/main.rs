31c47593d1ca111741e4ccba8bc2f1d7d9ec23dd
Ruben Acevedo <rubenfsolorzano@hotmail.com>
2022-11-16 18:35:40 -0300

    add termion

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate termion;
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+pub fn error_handler(error: std::io::Error) {
+    panic!("{}", error);
+}
 fn main() {
-    println!("Hello world");
+    let _stdout = stdout().into_raw_mode().unwrap();
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => {
+                    if c.is_control() {
+                        println!("{:?}", c as u8);
+                    } else {
+                        println!("{:?} ({})\r", c as u8, c);
+                    }
+                }
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
+            Err(err) => println!("{}", err),
+        }
+    }
 }
